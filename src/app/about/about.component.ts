4d4636dc5979880141cc603db52cf1ae8eaf0f2b
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-03-15 13:31:10 +0100

    Load README from my GitHub repo

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.sass']
 })
 export class AboutComponent implements OnInit {
+  readme = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getReadmeRaw()
+      .pipe(mergeMap(text => this.renderMarkdown(text)))
+      .subscribe(response => this.readme = response);
+  }
+
+  private renderMarkdown(text: string): Observable<string> {
+    return this.http.post('https://api.github.com/markdown', {text}, { responseType: 'text' });
+  }
+
+  private getReadmeRaw(): Observable<string> {
+    return this.http.get('https://raw.githubusercontent.com/aerabi/aerabi/master/README.md', {responseType: 'text'});
   }
 
 }
