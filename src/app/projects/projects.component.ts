54cb74f6983e89308deee28c3b060d44d3c11d86
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2022-08-05 00:52:00 +0200

    Feat: Replace 2nd and 3rd tabs with blog and talks

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.sass']
 })
 export class ProjectsComponent implements OnInit {
+  readme = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getReadmeRaw()
+      .pipe(mergeMap(text => this.renderMarkdown(text)))
+      .subscribe(response => this.readme = response);
+  }
+
+  private renderMarkdown(text: string): Observable<string> {
+    return this.http.post('https://api.github.com/markdown', {text}, { responseType: 'text' });
+  }
+
+  private getReadmeRaw(): Observable<string> {
+    return this.http.get('https://raw.githubusercontent.com/aerabi/git-weekly/master/README.md', {responseType: 'text'});
   }
 
 }
